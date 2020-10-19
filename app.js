4d95b95f99a54a799a48829aa39aa14e17df2bb4
BentleyJensen <bentleyjensen@users.noreply.github.com>
2020-10-18 22:56:41 -0700

    Add page views & routes

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -19,8 +16,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Set Router files
+var indexRouter = require('./routes/index');
+var libraryRouter = require('./routes/library');
+
+// Create Routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/library', libraryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
