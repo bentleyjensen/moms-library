6273eab069714f31bedfea2c85349ac992c5c7ea
BentleyJensen <bentleyjensen@users.noreply.github.com>
2020-10-21 19:02:01 -0700

    Fix links

--- app.js
+++ app.js
@@ -19,10 +19,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Set Router files
 var indexRouter = require('./routes/index');
 var libraryRouter = require('./routes/library');
+var adminRouter = require('./routes/admin');
+var loginRouter = require('./routes/login');
+var checkinRouter = require('./routes/checkin');
+var checkoutRouter = require('./routes/checkout');
+var searchRouter = require('./routes/search');
+var studentAccountRouter = require('./routes/studentAccount');
+var reportsRouter = require('./routes/reports');
+var addBooksRouter = require('./routes/addBooks');
+var adminSearchRouter = require('./routes/adminSearch');
+var adminStudentsRouter = require('./routes/adminStudents');
+
 
 // Create Routes
 app.use('/', indexRouter);
 app.use('/library', libraryRouter);
+app.use('/admin', adminRouter);
+app.use('/admin/login', loginRouter);
+app.use('/checkin', checkinRouter);
+app.use('/checkout', checkoutRouter);
+app.use('/search', searchRouter);
+app.use('/studentAccount', studentAccountRouter);
+app.use('/admin/reports', reportsRouter);
+app.use('/admin/addBooks', addBooksRouter);
+app.use('admin/Search', adminSearchRouter);
+app.use('/admin/Students', adminStudentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
